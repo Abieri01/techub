5bb6f4e8a8f2cf93d2cb4cfd7612180fce72b80d
MatheusAbade513 <matheusabade2@gmail.com>
2024-01-26 14:23:19 -0300

    atualizado

---
+++ src/src/app/Pages/perfil/perfil.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PerfilPageRoutingModule } from './perfil-routing.module';
+
+import { PerfilPage } from './perfil.page';
+import { register } from 'swiper/element/bundle';
+
+register();
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    PerfilPageRoutingModule
+  ],
+  declarations: [PerfilPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class PerfilPageModule {}
