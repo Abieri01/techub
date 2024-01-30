95d815fb808afd2638b15813ab6ce547436099fc
albuquerque14 <127431207+albuquerque14@users.noreply.github.com>
2024-01-30 13:55:41 -0300

    vitor

--- src/app/Components/login-form/login-form.component.ts
+++ src/app/Components/login-form/login-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signOut } from '@angular/fire/auth';
+import { Firestore, addDoc, collection } from '@angular/fire/firestore';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -8,89 +9,112 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent implements OnInit {
-  cad: boolean = false
-  mensagem: string = ''
-  logado: boolean = false
-  isToastOpen = false
-  user:any={nome:'',foto:''}
+  email: string = '';
+  uid: string = '';
+  cad: boolean = false;
+  cadPessoal: boolean = false;
+  mensagem: string = '';
+  logado: boolean = false;
+  isToastOpen = false;
+  user: any = { nome: '', foto: '' };
+  nome: string = '';
+  sobrenome: string = '';
+  sexo: string = '';
+  cpf: string = '';
+  nascimento: string = '';
+  telefone: string = '';
+  cep: string = '';
+  rua: string = '';
+  numero: string = '';
+  complemento: string = '';
+  referencia: string = '';
+  bairro: string = '';
+  cidade: string = '';
+  estado: string = '';
+
   setOpen(isOpen: boolean) {
     this.isToastOpen = isOpen;
   }
-  cadUser(email: any, senha: any, rpSenha: any) {
-    this.mensagem = ''
-    if (email == '' || senha == '' || rpSenha == '') {
-      this.mensagem = 'Preencha todos os campos do formulário!'
-      this.setOpen(true)
-    } else if (senha != rpSenha) {
-      this.mensagem = 'As senhas precisam ser iguas!'
-      this.setOpen(true)
-    } else {
-      this.mensagem = 'Usuário cadastrado com sucesso!'
-      this.setOpen(true)
-      this.cad = !this.cad
 
+  cadUser(email: any, senha: any, rpSenha: any) {
+    this.mensagem = '';
+    if (email == '' || senha == '' || rpSenha == '') {
+      this.mensagem = 'Preencha todos os campos do formulário!';
+      this.setOpen(true);
+    } else if (senha != rpSenha) {
+      this.mensagem = 'As senhas precisam ser iguais!';
+      this.setOpen(true);
+    } else {
+      this.mensagem = 'Usuário cadastrado com sucesso!';
+      this.setOpen(true);
+      this.cad = !this.cad;
       createUserWithEmailAndPassword(this.auth, email, senha)
         .then((userCredential) => {
-          // Signed up 
+          console.log('test1')
           const user = userCredential.user;
-          // ...
-          // Adicione a mensagem de sucesso
-        this.mensagem = 'Cadastro realizado com sucesso!';
-        this.setOpen(true);
-
-        // Navegue para a página /perfil após o cadastro bem-sucedido
-        this.navCtrl.navigateForward('/cadastro');
-      })
+          sessionStorage.setItem('email', email);
+          sessionStorage.setItem('uid', user.uid);
+          this.uid = user.uid;
+          this.email = email
+          this.mensagem = 'Cadastro realizado com sucesso!';
+          this.setOpen(true);
+          this.navCtrl.navigateForward('/cadastro'); 
+          //this.salvarNoFirestore();  // Chame a função de salvar no Firestore
+        })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          // ..
+          console.error('Erro ao cadastrar: ', errorMessage);
         });
     }
-
   }
-
   Logar(email: any, senha: any) {
     signInWithEmailAndPassword(this.auth, email, senha)
       .then((userCredential) => {
         const user = userCredential.user;
-        console.log(user.email)
-        this.mensagem = `Usuário: ${user.email} logado com sucesso!`
-        this.setOpen(true)
-        this.logado = !this.logado
+        console.log(user.email);
+        this.mensagem = `Usuário: ${user.email} logado com sucesso!`;
+        this.setOpen(true);
+        this.logado = !this.logado;
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
+        console.error('Erro ao logar: ', errorMessage);
       });
   }
 
   logout() {
-    this.mensagem = 'LogOut efetuado com sucesso!'
-    this.setOpen(true)
-    this.logado = !this.logado
-    this.logOutComGoogle()
+    this.mensagem = 'LogOut efetuado com sucesso!';
+    this.setOpen(true);
+    this.logado = !this.logado;
+    this.logOutComGoogle();
   }
+
   loginComGoogle() {
-    const provider = new GoogleAuthProvider()
+    const provider = new GoogleAuthProvider();
     signInWithPopup(this.auth, provider)
       .then((result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
-        this.mensagem = `Usuário: ${result.user.displayName} logado com sucesso!`
-        this.user.nome=result.user.displayName
-        this.user.foto=result.user.photoURL
-        this.setOpen(true)
-        this.logado = !this.logado
-      }).catch((error) => {
+        this.mensagem = `Usuário: ${result.user.displayName} logado com sucesso!`;
+        this.user.nome = result.user.displayName;
+        this.user.foto = result.user.photoURL;
+        this.setOpen(true);
+        this.logado = !this.logado;
+      })
+      .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         const email = error.customData.email;
         const credential = GoogleAuthProvider.credentialFromError(error);
+        console.error('Erro ao logar com Google: ', errorMessage);
       });
   }
-  logOutComGoogle(){
-    return signOut(this.auth)
+  logOutComGoogle() {
+    return signOut(this.auth);
   }
+
+
 
   public alertButtons = [
     {
@@ -107,11 +131,10 @@ export class LoginFormComponent implements OnInit {
 
   constructor(private auth: Auth, private navCtrl: NavController) { }
 
+
+ 
+
+  constructor(private auth: Auth, private firestore: Firestore, private navCtrl: NavController) { }
+
   ngOnInit() { }
-
 }
-
-
-
-
-
