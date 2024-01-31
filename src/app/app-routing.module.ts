b4b88139d653af4ad541d08b12b374d176f68cbb
albuquerque14 <127431207+albuquerque14@users.noreply.github.com>
2024-01-31 16:20:59 -0300

    Vitor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,6 +43,14 @@ const routes: Routes = [
   { 
     path: 'cadastro', 
     loadChildren: () => import('./Pages/cadastro/cadastro.module').then(m => m.CadastroPageModule) 
+  },
+  {
+    path: 'pedidos-suporte',
+    loadChildren: () => import('./Pages/pedidos-suporte/pedidos-suporte.module').then( m => m.PedidosSuportePageModule)
+  },
+  {
+    path: 'departamentos',
+    loadChildren: () => import('./Pages/departamentos/departamentos.module').then( m => m.DepartamentosPageModule)
   }
 ];
 
